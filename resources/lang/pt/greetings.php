81aadfbb1547bbb4ea55c6da356c884afde13f82
MrWillian <willian.dev10@gmail.com>
2021-04-13 10:07:52 -0300

    chore: increase greetings file pt language

---
+++ resources/lang/pt/greetings.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Greetings Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during greetings for various
+    | messages that we need to display to the user.
+    |
+    */
+
+    'Welcome to Net Express System' => 'Seja bem-vindo ao Sistema Net Express',
+    'title' => 'Net Express System',
+
+];
