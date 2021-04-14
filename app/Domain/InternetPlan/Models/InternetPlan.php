4dc09226c5c6a0537661581edb1b21a7e4303d89
MrWillian <willian.dev10@gmail.com>
2021-04-14 20:59:14 -0300

    chore: increase internet plan model

---
+++ app/Domain/InternetPlan/Models/InternetPlan.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Domain\InternetPlan\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InternetPlan extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+}
