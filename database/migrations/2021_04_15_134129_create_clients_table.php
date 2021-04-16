8c5754ccc5cf3f4cb269963023e73f6a062b929e
MrWillian <willian.dev10@gmail.com>
2021-04-15 22:15:42 -0300

    chore: increase clients table migration

---
+++ database/migrations/2021_04_15_134129_create_clients_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('email', 128);
+            $table->date('birthday');
+            $table->string('phone_number', 20);
+            $table->enum('client_type', [1, 2]);
+            $table->enum('gender', ["M", "F"]);
+            $table->string('rg', 12);
+            $table->string('document', 18);
+            $table->string('cep', 11);
+            $table->string('country', 60);
+            $table->string('state', 60);
+            $table->string('city', 60);
+            $table->string('district', 60);
+            $table->string('place', 90);
+            $table->string('number', 10);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
