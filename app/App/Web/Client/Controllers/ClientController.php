bc09c771753d4f028ff008086d0efd9aacdf0fb2
MrWillian <willian.dev10@gmail.com>
2021-04-28 15:41:00 -0300

    chore: remove unused methods

--- app/App/Web/Client/Controllers/ClientController.php
+++ app/App/Web/Client/Controllers/ClientController.php
@@ -31,20 +31,6 @@ class ClientController extends Controller
     }
 
     /**
-     * Store a newly created client in storage
-     *
-     * @param  \Web\Client\Requests\ClientRequest $request
-     * @param  \Domain\Client\Actions\CreateClientAction $action
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(ClientRequest $request, CreateClientAction $action)
-    {
-        dd($request);
-        $action(ClientData::fromRequest($request));
-        return redirect()->route('clients.index')->with(['success' => __('Client successfully created.')]);
-    }
-
-    /**
      * Show the form for editing the specified client
      *
      * @param  \Domain\Client\Models\Client $client
@@ -53,38 +39,5 @@ class ClientController extends Controller
     public function edit(Client $client)
     {
         return view('clients.edit', ['client' => $client->load('role')]);
-    }
-
-    /**
-     * Update the specified user in storage
-     *
-     * @param  \App\Http\Requests\UserRequest  $request
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function update(UserRequest $request, User $user)
-    {
-        $hasPassword = $request->get('password');
-        $user->update(
-            $request->merge([
-                'picture' => $request->photo ? $request->photo->store('profile', 'public') : $user->picture,
-                'password' => Hash::make($request->get('password'))
-            ])->except([$hasPassword ? '' : 'password'])
-        );
-
-        return redirect()->route('user.index')->withStatus(__('User successfully updated.'));
-    }
-
-    /**
-     * Remove the specified user from storage
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(User $user)
-    {
-        $user->delete();
-
-        return redirect()->route('user.index')->withStatus(__('User successfully deleted.'));
     }
 }
