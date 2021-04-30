919e2a68ccdff2dd8b6898a45d08cc0de01ca46a
MrWillian <willian.dev10@gmail.com>
2021-04-29 21:08:26 -0300

    fix: fix route to client index

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -19,7 +19,7 @@
               <div class="card-body ">
                 <div class="row">
                   <div class="col-md-12 text-right">
-                      <a href="{{ route('client.index') }}" class="btn btn-sm btn-rose">{{ __('Back to list') }}</a>
+                      <a href="{{ route('web.client.index') }}" class="btn btn-sm btn-rose">{{ __('Back to list') }}</a>
                   </div>
                 </div>
                 <div class="row">
